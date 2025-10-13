156f7fbda7d55a0b491f2449817f40f51fa99f01
Onyeanya Harmonie <harmonie.onyeanya@pau.edu.ng>
2025-10-13 17:45:55 +0100

    (Week 3 practice)

---
+++ week-3/practice_4/target/debug/practice_4.d
@@ -0,0 +1,1 @@
+C:\Users\Onyeanya\ Harmonie\Documents\h.onyeanyaCOS101\week-3\practice_4\target\debug\practice_4.exe: C:\Users\Onyeanya\ Harmonie\Documents\h.onyeanyaCOS101\week-3\practice_4\src\main.rs
---
+++ week-3/practice_6/target/debug/deps/practice_6.d
@@ -0,0 +1,5 @@
+C:\Users\Onyeanya Harmonie\Documents\h.onyeanyaCOS101\week-3\practice_6\target\debug\deps\practice_6.d: src\main.rs
+
+C:\Users\Onyeanya Harmonie\Documents\h.onyeanyaCOS101\week-3\practice_6\target\debug\deps\practice_6.exe: src\main.rs
+
+src\main.rs:
