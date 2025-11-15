f8fc2bfd04f1b1e9ec5dd71af12c70f720705341
Onyeanya Harmonie <harmonie.onyeanya@pau.edu.ng>
2025-11-15 21:09:57 +0100

    (project1)

--- practices/study_1/src/main.rs
+++ practices/study_1/src/main.rs
@@ -1,7 +1,8 @@
-
 fn main() {
- loop{
-    println!("This will repeat forever!");
+ let mut count =1;
+ while count<10{
+    println!("count is {}",count );
+    count+=1
  }
+ println!("hggygyhgh {}",count );
 }
- 
---
+++ week-7/project_1/target/debug/project_1.d
@@ -0,0 +1,1 @@
+C:\Users\Onyeanya\ Harmonie\Documents\h.onyeanyaCOS101\week-7\project_1\target\debug\project_1.exe: C:\Users\Onyeanya\ Harmonie\Documents\h.onyeanyaCOS101\week-7\project_1\src\main.rs
