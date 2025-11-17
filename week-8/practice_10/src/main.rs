15298fa096efccae18c6cb27b388240ddddf3b17
Onyeanya Harmonie <harmonie.onyeanya@pau.edu.ng>
2025-11-17 23:16:12 +0100

    (practice10)

---
+++ week-8/practice_10/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    
+    let b:( i32,bool,f64) = (110,true,10.9);
+    print(b);
+
+}
+
+fn print(x:(i32,bool,f64) ) {
+    println!("Inside print method");
+    //assigna tuple to distinct variables
+    let (age, is_male,cgpa) = x;
+    println!("Age is {}, is male? {}, CGPA is {}",age,is_male,cgpa);
+}
---
+++ week-8/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\Onyeanya\ Harmonie\Documents\h.onyeanyaCOS101\week-8\practice_10\target\debug\practice_10.exe: C:\Users\Onyeanya\ Harmonie\Documents\h.onyeanyaCOS101\week-8\practice_10\src\main.rs
