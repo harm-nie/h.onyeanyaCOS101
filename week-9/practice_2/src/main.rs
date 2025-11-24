50550bbbb0571400b7689e15c4918705d9c748e3
Onyeanya Harmonie <harmonie.onyeanya@pau.edu.ng>
2025-11-24 09:45:59 +0100

    (practice2)

--- week-9/practice_2/src/main.rs
+++ week-9/practice_2/src/main.rs
@@ -3,15 +3,6 @@ use std::io::Read;
 fn main(){
 
     
-let announce = "Week 9 - Rust File Input & Output\n";
-    let dept = "Department of Computer Science";
-
-    let mut file = std::fs::File::create("data.txt").expect("create failed");
-    file.write_all("Welcome to Rust Programming\n"
-        .as_bytes()).expect("write failed");
-    file.write_all(announce.as_bytes()).expect("write failed");
-    file.write_all(dept.as_bytes()).expect("write failed");
-    println!("\nData written to file.");
     
     let mut file = std::fs::File::open("welcome_message.txt").unwrap();
     let mut contents = String::new();
