6dbde4b73b2bf2c9842195adc4ff02fef3aea0ea
Onyeanya Harmonie <harmonie.onyeanya@pau.edu.ng>
2025-12-07 11:24:12 +0100

    (practice8)

---
+++ week-10/practice_8/src/main.rs
@@ -0,0 +1,31 @@
+//declare a structure
+struct Employee {
+    ceo:String,
+    company:String,
+    age:u32
+}
+
+fn main() {
+    //initialize a structure
+    let emp1 = Employee {
+        company:String::from("Microsoft Corporation"),
+        ceo:String::from("Satya Nadella"),
+        age:56
+    };
+    
+    let emp2 = Employee{
+        company:String::from("Google Inc."),
+        ceo:String::from("Sundai Pichal"),
+        age:51
+    };
+    
+    
+    display(emp1);
+    display(emp2);
+
+    
+    fn display(emp:Employee){
+        println!("Name is :{} company is {} age is 
+        {}",emp.ceo,emp.company,emp.age);
+    }
+}
